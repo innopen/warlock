b19e406b49483048a282b84123d06a7a53ba8269
Wizard Technolab <wizardtechnolab@gmail.com>
2021-05-13 17:25:36 +0530

    [ADD] added code for dynamic attribute

--- wt_product_variant_popup/controllers/main.py
+++ wt_product_variant_popup/controllers/main.py
@@ -1,27 +1,31 @@
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo import http
 from odoo.http import request
+import itertools
 
 
 class WebsiteSale(WebsiteSale):
     @http.route(['/sale/get_list_combination_info'], type='json', auth="public", methods=['POST'], website=True)
     def get_combination_list_info(self, product_template_id, pricelist_id, **kw):
-        product_templ_ids = request.env['product.template'].browse(product_template_id)
+        product_templ_id = request.env['product.template'].browse(product_template_id)
         pricelist = request.website.get_current_pricelist()
-        product_variant_ids = product_templ_ids.product_variant_ids
         datas = []
         values = {}
-        for product in product_variant_ids:
-            product_datas = {}
-            if product.exists():
-                combination = product.product_template_attribute_value_ids
-                is_combination_possible = product_templ_ids._is_combination_possible(combination=combination, parent_combination=False)
-                if is_combination_possible:
-                    product_datas = product_templ_ids._get_combination_info(combination, int(product.id or 0), int(1), pricelist)
-                    product_datas['is_combination_possible'] = is_combination_possible
-                    product_datas['product_att_ids'] = product.product_template_attribute_value_ids.ids
-            if product_datas:
-                datas.append(product_datas)
+        product_datas = {}
+        if product_templ_id.exists():
+            all_combinations = itertools.product(*[
+                ptal.product_template_value_ids._only_active() for ptal in product_templ_id.valid_product_template_attribute_line_ids._without_no_variant_attributes()
+            ])
+            for combination_tuple in all_combinations:
+                combination = request.env['product.template.attribute.value'].concat(*combination_tuple)
+                if not combination.filtered(lambda l: l.exclude_for):
+                    is_combination_possible = product_templ_id._is_combination_possible_by_config(combination=combination, ignore_no_variant=True,)
+                    if is_combination_possible:
+                        product_datas = product_templ_id._get_combination_info(combination, int(product_templ_id.id or 0), int(1), pricelist)
+                        product_datas['is_combination_possible'] = is_combination_possible
+                        product_datas['product_att_ids'] = combination.ids
+                    if product_datas:
+                        datas.append(product_datas)
         values['varinat_popup'] = request.env['ir.ui.view']._render_template("wt_product_variant_popup.variant_popup", {
             'datas': datas[0:3],
             'website': request.website,
